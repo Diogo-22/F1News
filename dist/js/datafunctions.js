14a3ae59fcdafc613d901d41bd8c681bbdce7316
Diogo <diogo.peralta.goncalves@gmail.com>
2023-05-10 17:47:15 +0100

    added standings page

--- dist/js/datafunctions.js
+++ dist/js/datafunctions.js
@@ -30,3 +30,43 @@ export const getGPWinners = async (selectedYear, selectedRound) => {
         console.error(err);
     }
 }
+
+export const getStandings = async (selectedYear) => {
+    const selectYear = selectedYear;
+    const url = `https://ergast.com/api/f1/${selectYear}/driverStandings.json`;
+    /* const url = `https://ergast.com/api/f1/2021/driverStandings.json`; */
+    try {
+        const getStandings = await fetch(url);
+        
+        const standingsJson = await getStandings.json();
+        console.log(standingsJson);
+        const standings = standingsJson.MRData.StandingsTable.StandingsLists[0].DriverStandings;
+        const standingsLength = standings.length;
+        //console.log(standingsLength)
+        const positionArray = [];
+        const pointsArray = [];
+        const winsArray = [];
+        const driversNameArray = [];
+        const teamArray = [];
+        for (let i = 0; i < standingsLength; i++) {
+            const position = standings[i].position;
+            positionArray[i] = position;
+            const points = standings[i].points;
+            pointsArray[i] = points;
+            const wins = standings[i].wins;
+            winsArray[i] = wins;
+            const givenName = standings[i].Driver.givenName;
+            const lastName = standings[i].Driver.familyName;
+            const driverName = givenName + " " + lastName;
+            driversNameArray[i] = driverName;
+            const team = standings[i].Constructors[0].name;
+            teamArray[i] = team;
+            
+        }
+        
+        //console.log(WinnerJson);
+        return [positionArray, driversNameArray, teamArray, pointsArray, winsArray, standingsLength];
+    } catch (err) {
+        console.error(err);
+    }
+}
---
+++ dist/js/standingsmain.js
@@ -0,0 +1,33 @@
+
+import {
+    createStandingsTable,
+    dropdownyearList
+} from "./standingsdomfunctions.js"
+import {
+    getStandings
+} from "./datafunctions.js"
+const initApp = () => {
+    console.log("hello there")
+    dropdownyearList();
+    let selectedYear = new Date().getFullYear();
+    console.log(selectedYear)
+   const yearButton = document.getElementById("year-dropdown2");
+    yearButton.addEventListener("change", async function() {
+        const selectedOption = this.value;
+        selectedYear = selectedOption;
+        standings(selectedYear)
+      });
+    standings(selectedYear);
+    //console.log(getStandings())
+   
+
+
+}
+document.addEventListener("DOMContentLoaded", initApp);
+
+const standings = async (year) => {
+
+    const arrays = await getStandings(year);
+    console.log(arrays)
+    createStandingsTable(arrays);
+}
